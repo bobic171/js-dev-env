a60c2d3cbe644bcddbabe99b24954a9ab453a8c0
bobic171 <bobic171@gmail.com>
2018-01-02 20:54:01 +0000

    fix webpack.config and linting watch

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const busTicket = numeral(1000).format('£0,0.00');
-console.log(`The bus ticket is  ${busTicket} to London.`);
+console.log(`The bus ticket is ${busTicket} to London.`); // eslint-disable-line no-console
