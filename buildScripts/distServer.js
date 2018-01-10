d8ede09b1f4682aa57e7e5a48c283a35e75a99a3
bobic171 <bobic171@gmail.com>
2018-01-10 18:14:29 +0000

    production build scripts

---
+++ buildScripts/distServer.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/users', function(req, res){
+  // Hard coded demo api data
+  res.json([
+    {"id": 1,"firstName":"Adam","lastName":"Farquhar","email":"adam@gmail.com"},
+    {"id": 2,"firstName":"Dave","lastName":"Smith","email":"dave@gmail.com"},
+    {"id": 3,"firstName":"Gabi","lastName":"Brown","email":"gabi@gmail.com"}
+  ]);
+});
+
+app.listen(port, function(err){
+  if(err){
+    console.log(err);
+  }else{
+    open('http://localhost:' + port);
+  }
+});
