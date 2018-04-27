52654a34ba8917cd14382f690835e2258a233906
bobic171 <bobic171@gmail.com>
2018-04-27 22:23:27 +0100

    Tidy up env for easyer use

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,14 +15,14 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function(req, res){
-  // Hard coded demo api data
-  res.json([
-    {"id": 1,"firstName":"Adam","lastName":"Farquhar","email":"adam@gmail.com"},
-    {"id": 2,"firstName":"Dave","lastName":"Smith","email":"dave@gmail.com"},
-    {"id": 3,"firstName":"Gabi","lastName":"Brown","email":"gabi@gmail.com"}
-  ]);
-});
+// app.get('/users', function(req, res){
+//   // Hard coded demo api data
+//   res.json([
+//     {"id": 1,"firstName":"Adam","lastName":"Farquhar","email":"adam@gmail.com"},
+//     {"id": 2,"firstName":"Dave","lastName":"Smith","email":"dave@gmail.com"},
+//     {"id": 3,"firstName":"Gabi","lastName":"Brown","email":"gabi@gmail.com"}
+//   ]);
+// });
 
 app.listen(port, function(err){
   if(err){
